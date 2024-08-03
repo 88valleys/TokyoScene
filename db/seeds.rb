9b4c55ae1ba17d6d076014a35f447986cd5b3689
Xinyi Wang <rena.wxy@gmail.com>
2024-08-03 17:58:03 +0900

    the seeds work

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+Gig.destroy_all
+User.destroy_all
+
+puts "Destroying all gigs..."
+puts "Destroying all users..."
+
+address = ["Asakusa", "Shibuya", "Odaiba", "Shinjuku", "Ginza", "Meguro"]
+
+10.times do |i|
+  user = User.create!(email: "user-#{i + 1}@example.com", password: "123456")
+  Gig.create!(
+    user: user,
+    name: Faker::Music::RockBand.name,
+    time: Faker::Date.between(from: Date.today, to: '2025-12-31'),
+    description: Faker::Quotes::Shakespeare.hamlet_quote,
+    location: address.sample
+  )
+end
+
+puts "Created #{User.count} users!"
+puts "Created #{Gig.count} gigs!"
