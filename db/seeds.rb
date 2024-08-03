3e0b7f800324d174e4d269dae405136f21910a65
Dianna Antenucci <diannarose9@yahoo.com>
2024-08-03 18:06:01 +0900

    made the index but changed all seeds rb as well

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+
+Gig.destroy_all
+puts "Destroying all gigs..."
+User.destroy_all
+puts "Destroying all users..."
+
+address = ["Asakusa", "Shibuya", "Odaiba", "Shinjuku", "Ginza", "Meguro"]
+gig_name = ["TomorrowLand", "Fuji Rock", "Lalapaloza"]
+fan_user = User.create!(email: "fan@example.com", password: "123456")
+band_user = User.create!(email: "band@example.com", password: "123456", is_band: true, band_name: "RockBand")
+
+2.times do |i|
+  Gig.create!(
+    user: band_user,
+    name: gig_name.sample,
+    time: Faker::Date.between(from: Date.today, to: '2025-12-31'),
+    description: Faker::Lorem.paragraph,
+    location: address.sample
+  )
+end
+
+puts "Created #{User.count} users!"
+puts "Created #{Gig.count} gigs!"
