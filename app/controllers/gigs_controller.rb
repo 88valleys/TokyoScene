6d5c36221efc8328080658ae8e1d4e7895a44dbb
Senie Calalang <senie.c88@gmail.com>
2024-08-07 00:12:35 +0900

    WIP: added map and markers to the home.html.erb and gigs/index.html.erb

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,2 +1,12 @@
 class GigsController < ApplicationController
+  def index
+    @gigs = Gig.all
+    @markers = @gigs.geocoded.map do |gig|
+      {
+        lat: gig.latitude,
+        lng: gig.longitude,
+        info_window_html: render_to_string(partial: "layouts/info_window", locals: {gig: gig})
+      }
+    end  
+  end
 end
--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,3 +1,5 @@
 class Gig < ApplicationRecord
   belongs_to :user
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
