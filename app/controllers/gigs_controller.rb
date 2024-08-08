84eb4352b5e939b79ae7a0f6063a74cf1a520bb9
Dianna Antenucci <diannarose9@yahoo.com>
2024-08-08 22:05:11 +0900

    gigs html was fixed and registration html and info button was added

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -3,4 +3,8 @@ class GigsController < ApplicationController
     def index
         @gigs = Gig.all
     end
+
+    def show
+        @gig = Gig.find(params[:id])
+    end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,6 @@
 class RegistrationsController < ApplicationController
+    def new
+        @gig = Gig.find(params[:gig_id])
+        @registration = @gig.registrations.new
+    end
 end
