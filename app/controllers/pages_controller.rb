0ccade512677821ddf701b1d53af688ad1919675
Dianna Antenucci <diannarose9@yahoo.com>
2024-08-10 17:23:52 +0900

    changed button that said test to say register

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,7 +20,7 @@ class PagesController < ApplicationController
       {
         lat: gig.latitude,
         lng: gig.longitude,
-        info_window_html: render_to_string(partial: "gigs/gigs", locals: { gig:gig})
+        info_window_html: render_to_string(partial: "gigs/gigs", locals: { gig: gig }),
       }
     end
   end
