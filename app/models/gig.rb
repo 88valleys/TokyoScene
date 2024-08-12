5a8ac8324891f11a627757b47c5d722b53a16f18
Senie Calalang <senie.c88@gmail.com>
2024-08-13 02:13:00 +0900

    gig model updated with name changed to band for more clarityin the gig model

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,18 +1,30 @@
+
 class Gig < ApplicationRecord
   belongs_to :user
   acts_as_taggable_on :genre
 
   # search form
   include PgSearch::Model
-  pg_search_scope :search_by_name_and_description_and_location,
-  against: [ :name, :description, :location  ],
+  pg_search_scope :search_by_band_and_description_and_location,
+  against: [ :band, :description, :location  ],
   using: {
     tsearch: { prefix: true }
   }
   has_many :registrations
   has_many :messages
 
-  validates :name, :time, :user, :location, presence: true
+  validates :band, :time, :user, :location, presence: true
+
+  # Gets the band's image from Spotify's API and places it in the Gig model
+
+  def fetch_band_image
+    band_info = RSpotify::Artist.search(self.band).first
+    if band_info && band_info.images.any?
+      self.band_image_url = band_info.images.first['url']
+    else
+      self.band_image_url = 'https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg'
+    end
+  end
 
   # geocoded_by :location
 
