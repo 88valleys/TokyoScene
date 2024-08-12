cbbd463439e21e03b40f93e71d6536ff8cc11a97
Senie Calalang <senie.c88@gmail.com>
2024-08-13 02:11:47 +0900

    added spotify api - band images functionality - added fetch_band_image method within def create to gigs

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,4 +1,24 @@
 class GigsController < ApplicationController
+  def create
+    @gig = Gig.new(gig_params)
+    if @gig.save
+      @gig.fetch_band_image
+      redirect_to @gig, notice: 'Gig was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @gig = Gig.find(params[:id])
+    if @gig.update(gig_params)
+      @gig.fetch_band_image
+      redirect_to @gig, notice: 'Gig was successfully updated.'
+    else
+      render :edit
+    end
+  end
+  
   def show
     @gig = Gig.find(params[:id])
     @registration = Registration.new
@@ -6,5 +26,9 @@ class GigsController < ApplicationController
 
   def index
     @gigs = Gig.all
+  end
+
+  def gig_params
+    params.require(:gig).permit(:band, :description, :location, :band_image_url)
   end
 end
