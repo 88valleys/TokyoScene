a88cc6bd983a3a642a99c2ae35a8fd8d4136e48a
Yoosun Jang <yoosunjang0915@gmail.com>
2024-08-13 20:36:24 +0900

    changed user model to add favourite artists

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@ class User < ApplicationRecord
   # has_many :registrations
   # has_many :messages
   acts_as_taggable_on :genres
-
+  acts_as_taggable_on :artists
   has_many :registrations
   has_many :gigs, through: :registrations
+  has_many :artist_favourites
+  has_many :artists, through: :artist_favourites
 end
