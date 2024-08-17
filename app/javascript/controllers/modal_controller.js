d79c2bf7e2890d6fb4d7ddd42b993fa5b74e7136
Dianna Antenucci <diannarose9@yahoo.com>
2024-08-17 15:49:01 +0900

    modal was created but needs css and styling

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+
+// Connects to data-controller="modal"
+export default class extends Controller {
+  static targets = ["modalBody"]
+  static values = { id: Number }
+  connect() { console.log("Modal controller connected") 
+  }
+
+get_modal(event) {
+  console.log(this.idValue)
+  console.log(event.target)
+  const parser = new DOMParser()
+  fetch(`http://127.0.0.1:3000/gigs/${this.idValue}`)
+   .then(response => response.text())
+   .then((data) => {
+      const gigData = parser.parseFromString(data, "text/html")
+      const modal = gigData.querySelector(".confirm-card")
+      this.modalBodyTarget.innerHTML = modal.innerHTML
+    console.log(data)
+   })
+}
+}
