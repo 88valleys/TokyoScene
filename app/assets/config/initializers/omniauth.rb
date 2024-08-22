5ea1d29c506138c0712a722ca4a365d6ae80911d
Senie Calalang <senie.c88@gmail.com>
2024-08-22 23:13:23 +0900

    feat: Add Spotify authentication callback and update user sign-in - OAuth allows :post and :get request methods

--- app/assets/config/initializers/omniauth.rb
+++ app/assets/config/initializers/omniauth.rb
@@ -12,3 +12,5 @@ require "rspotify/oauth"
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: "user-read-email user-library-read"
 end
+
+OmniAuth.config.allowed_request_methods = [:post, :get]
