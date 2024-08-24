deea55f7aa016deaabef68386ce8a08064374b60
Dianna Antenucci <diannarose9@yahoo.com>
2024-08-24 12:17:38 +0900

    adding redis for heroku chat feature

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,8 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+# config/environments/production.rb
+config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
+
 end
