73a4e340b11401614bf488a066095911966951ca
Xinyi Wang <rena.wxy@gmail.com>
2024-08-24 16:05:22 +0900

    I add 100 gigs

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,22 +13,122 @@ puts "Destroying all registrations..."
 puts "Destroying all gigs..."
 puts "Destroying all users..."
 
-address = [
-  "Asakusa, Edo-dori Avenue, Kaminarimon 2-chome, Taito, Tokyo, 111-0034, Japan S",
-  "Odaiba, ウエストプロムナード, Daiba 1-chome, Daiba, Minato, Tokyo, 135-8625, Japan",
-  "Toho Cinemas, 1, Kabukicho 1, Kabukicho, Shinjuku, Tokyo, 160-0021, Japan ",
-  "Ginza Six, 1, Ginza 6-chome, Chuo, Tokyo, 104-0061, Japan",
-  "Asagaya, Star Road, Asagaya kita 2, Asagaya kita, Kōenji, Suginami, Tokyo, 166-0004, Japan Station ",
-  "Akihabara Station, 6, Soto-Kanda 1-chome, 外神田, Chiyoda, Tokyo, 101-0021, Japan",
-  "Roppongi Hills, Mafu Tunnel, Roppongi 6-chome, Roppongi, Azabu, Minato, Tokyo, 106-6122, Japan ",
-  "Meiji Jingu Shrine, 1, Yoyogi Kamizono-chō, Shibuya, Tokyo, 151-8557, Japan",
-  "Ikebukuro, 池袋駅 西武南口, Minami-Ikebukuro 1-chome, South Ikebukuro, Toshima, Tokyo, 171-8569, Japan",
-  "Sangenjaya, Setagaya, Tokyo, 154-0024, Japan Quarter",
+livehouses = [
+  # Shibuya
+  { livehouse: "Axxcis-Club Axxcis SHINJUKU", address: "Hanamichi-dori Street, Kabukicho 1, Kabukicho, Shinjuku, Tokyo, 160-0021, Japan" },
+  { livehouse: "Circus Tokyo", address: "16, Shibuya 3, Shibuya, Tokyo, 150-0002, Japan" },
+  { livehouse: "Club Asia", address: "Rambling Street, Maruyamacho, Shibuya, Tokyo, 155-0002, Japan" },
+  { livehouse: "Contact", address: "12, Dogen-zaka Street, Dogenzaka 2, Dogenzaka, Shibuya, Tokyo, 150-0043, Japan" },
+  { livehouse: "Cyclone", address: "Penguin Street, Udagawacho, Shibuya, Tokyo, 150-0042, Japan" },
+  { livehouse: "Daitokai", address: "Matsudo Station Line, 本町, Matsudo, Chiba Prefecture, 271-0091, Japan" },
+  { livehouse: "DUO Music Exchange", address: "Rambling Street, Maruyamacho, Shibuya, Tokyo, 150-0043, Japan" },
+  { livehouse: "FabCafe", address: "1-22-7, Dogen-zaka Street, Maruyamacho, Shibuya, Tokyo, 150-0044, Japan" },
+  { livehouse: "Galaxy (Shibuya)", address: "5-27-7, Cat Street, Jingumae 6, Jingūmae, Shibuya, Tokyo, 150-0001, Japan" },
+  { livehouse: "Hobgoblin", address: "Omoide-dori Street, Dogenzaka 1, Dogenzaka, Shibuya, Tokyo, 150-0043, Japan" },
+  { livehouse: "Home", address: "Nakasugi-dori Avenue, Saginomiya 4-chome, Nakano, Tokyo, 165-0032, Japan" },
+  { livehouse: "Image Forum", address: "Shibuya, Tokyo, 150-0002, Japan" },
+  { livehouse: "Kitsune", address: "2-20-13, Meiji-dori Avenue, Higashi, Higashi 2, Higashi, Shibuya, Tokyo, 150-0011, Japan" },
+  { livehouse: "La Mama", address: "Shibuya Chuo-dori Street, Dogenzaka 1, Dogenzaka, Shibuya, Tokyo, 150-0043, Japan" },
+  { livehouse: "Line Cube Shibuya", address: "1, Udagawacho, Shibuya, Tokyo, 150-0771, Japan" },
+  { livehouse: "Lush", address: "6, Shinjuku 3, Shinjuku, Tokyo, 160-0022, Japan" },
+  { livehouse: "Music Bar 45", address: "Sakuragaokacho, Shibuya, Tokyo, 150-0031, Japan" },
+  { livehouse: "NHK Hall", address: "1, Jinnan 2, Jinnan, Shibuya, Tokyo, 150-8001, Japan" },
+  { livehouse: "NIGHTFLY", address: "Udagawacho, Shibuya, Tokyo, 150-8507, Japan" },
+  { livehouse: "Oath", address: "9, Shibuya 4, Shibuya, Tokyo, 150-0002, Japan" },
+  { livehouse: "Sankeys TYO", address: "Sarugakucho, Shibuya, Tokyo, 150-0033, Japan" },
+  { livehouse: "Shibuya Ax", address: "Wave-dori Street, Dogenzaka 1, Dogenzaka, Shibuya, Tokyo, 150-0043, Japan" },
+  { livehouse: "SHIBUYA REX", address: "Miyamasu-zaka Street, Shibuya 2, Shibuya, Tokyo, 150-8510, Japan" },
+  { livehouse: "The Aldgate", address: "Shibuya Center-gai Street, Udagawacho, Shibuya, Tokyo, 150-0042, Japan" },
+  { livehouse: "The Room", address: "15-19, Sakuragaokacho, Shibuya, Tokyo, 150-0031, Japan" },
+  { livehouse: "Tower Records Shibuya", address: "14, Jinnan 1, Jinnan, Shibuya, Tokyo, 150-0041, Japan" },
+  { livehouse: "Womb", address: "16, Maruyamacho, Shibuya, Tokyo, 150-0044, Japan" },
+  { livehouse: "WWW", address: "Spain-zaka Street, Udagawacho, Shibuya, Tokyo, 150-0042, Japan" },
+  { livehouse: "翠月 (MITSUKI)", address: "Dogen-zaka Street, Dogenzaka 1, Dogenzaka, Shibuya, Tokyo, 150-0044, Japan" },
+  # Asakusa
+  { livehouse: "Cuzn Homeground", address: "Hisago-dori, Asakusa 2-chome, Asakusa, Taito, Tokyo, 111-0032, Japan" },
+  { livehouse: "Infinity Books", address: "Asakusa-dori, Azumabashi 1, Azumabashi, Sumida, Tokyo, 130-0001, Japan" },
+  # Shinjuku
+  { livehouse: "8bit cafe", address: "御苑通り, Shinjuku 2, Shinjuku, Tokyo, 160-0022, Japan" },
+  { livehouse: "Antiknock", address: "Meiji-dori Avenue, Shinjuku 4, Shinjuku, Tokyo, 151-8580, Japan" },
+  { livehouse: "Cafe Lavanderia", address: "新宿2-12-9, Hanazono-dori, Shinjuku 2, Shinjuku, Tokyo, 160-0022, Japan" },
+  { livehouse: "Christon Cafè", address: "Yasukuni-dori Avenue, Shinjuku 5, Shinjuku, Tokyo, 160-0022, Japan" },
+  { livehouse: "Marz", address: "Ichiban-dori Street, Kabukicho 2, Kabukicho, Shinjuku, Tokyo, 160-0021, Japan" },
+  { livehouse: "Rosso", address: "4C's Bar Rosso, 1, Kabukicho 1, Kabukicho, Shinjuku, Tokyo, 160-0021, Japan" },
+  { livehouse: "Spincoaster", address: "四谷角筈線, Yoyogi 2, Yoyogi, Nishi Shinjuku, Shibuya, Tokyo, 151-8583, Japan" },
+  { livehouse: "Tokyo Opera City", address: "Yamate-dori Avenue, Honmachi 3, Nishi Shinjuku 3, Nishi Shinjuku, Shinjuku, Tokyo, 151-0071, Japan" },
+  { livehouse: "Tower Records Shinjuku", address: "1, Shinjuku 3, Shinjuku, Tokyo, 160-0022, Japan" },
+  { livehouse: "Wild Side Tokyo", address: "Yasukuni-dori, Shinjuku 5, Shinjuku, Tokyo, 160-0022, Japan" },
+  { livehouse: "Zirco Tokyo", address: "Kuyakusho-dori Avenue, Kabukicho 1, Kabukicho, Shinjuku, Tokyo, 160-0021, Japan" },
+  # Asagaya
+  { livehouse: "mogumogu Asagaya", address: "Pearl Center Shika, Asagaya Pearl Center Shopping Street, Asagaya minami 1, Asagaya minami, Kōenji, Suginami, Tokyo, 166-8578" },
+  { livehouse: "Gamuso", address: "Asagaya kita 2, Asagaya kita, Suginami, Tokyo, Japan" },
+  { livehouse: "Art Bar Ten", address: "Asagaya kita, Kōenji, Suginami, Tokyo, 166-0001, Japan" },
+  { livehouse: "COOL DREAD BAR", address: "Asagaya kita 3, Asagaya kita, Suginami, Tokyo, Japan" },
+  { livehouse: "Loft A", address: "Asagaya kita 1, Asagaya kita, Kōenji, Suginami, Tokyo, 166-0001, Japan" },
+  { livehouse: "Yellow Vision", address: "Asagaya kita 2, Asagaya kita, Suginami, Tokyo, Japan" },
+  { livehouse: "Next Sunday", address: "Asagaya Shinmeiguu, Nakasugi dori Ave., Asagaya kita 1, Asagaya kita, Kōenji, Suginami, Tokyo, 166-0001, Japan" },
+  # Akihabara
+  { livehouse: "秋葉原 CLUB GOODMAN", address: "Kanda-Sakumagashi, Chiyoda, Tokyo, Japan" },
+  { livehouse: "Akihabara Marz", address: "Kanda-Sakumacho 1-chome, Kanda-Sakumacho, Chiyoda, Tokyo, 102-0000, Japan" },
+  { livehouse: "Fukumori", address: "Kanda-Sudacho 1-chome, Kanda-Sudacho, Chiyoda, Tokyo, 101-0041, Japan" },
+  { livehouse: "Akihabara ZEST", address: "Soto-Kanda 2, 外神田, Chiyoda, Tokyo, 102-0000, Japan" },
+  { livehouse: "TOKYOITE! the music space", address: "Soto-Kanda 2, 外神田, Chiyoda, Tokyo, 102-0000, Japan" },
+  { livehouse: "NARU Jazz Livehouse", address: "Kanda-Surugadai 2-chome, Kanda-Surugadai, Chiyoda, Tokyo, 101-0062, Japan" },
+  { livehouse: "BECK アキバ ライブハウス リハーサルスタジオ セッションバー", address: "Kanda-Sakumacho 2-chome, Kanda-Sakumacho, Chiyoda, Tokyo, 101-0025, Japan" },
+  # Roppongi
+  { livehouse: "SUPER DELUXE", address: "Nishiazabu-yonchome, Kogai-zaka Street, Roppongi 6-chome, Nishi-Azabu 4-chome, Azabu, Minato, Tokyo, 106-0031, Japan" },
+  { livehouse: "BILLBOARD LIVE TOKYO", address: "Akasaka 9-chome, Akasaka, Azabu, Minato, Tokyo, 106-0032, Japan" },
+  { livehouse: "六本木 CLUB EDGE", address: "Roppongi 5-chome, Roppongi, Azabu, Minato, Tokyo, 106-8537, Japan" },
+  { livehouse: "EX THEATER ROPPONGI", address: "DUO SCALA NISHIAZABU TOWER, Roppongi-dori, Roppongi 6-chome, Nishi-Azabu 1-chome, Azabu, Minato, Tokyo, 106-0031, Japan" },
+  { livehouse: "BAUHAUS", address: "アロービル, 7, Roppongi, Azabu, Minato, Tokyo, 106-0032, Japan" },
+  { livehouse: "Jazz House ALFIE", address: "Roppongi 6-chome, Roppongi, Azabu, Minato, Tokyo, 106-8001, Japan" },
+  { livehouse: "Common", address: "六本木Dスクエア, 5, Roppongi, Azabu, Minato, Tokyo, 106-0032, Japan" },
+  # Ikebukuro
+  { livehouse: "Live garage Adm", address: "Higashi-Ikebukuro 1-chome, Toshima, Tokyo, 170-0013, Japan" },
+  { livehouse: "池袋 Live inn ROSA", address: "Nishi-Ikebukuro 1-chome, Toshima, Tokyo, 171-8504, Japan" },
+  { livehouse: "Absolute Blue", address: "Nishi-Ikebukuro 1-chome, Toshima, Tokyo, 171-8504, Japan" },
+  { livehouse: "Apple Jump", address: "Nishi-Ikebukuro 3-chome, Toshima, Tokyo, 171-0021, Japan" },
+  { livehouse: "KAKULULU", address: "Higashi-Ikebukuro 4-chome, Toshima, Tokyo, 170-6002, Japan" },
+  { livehouse: "INDEPENDENCE", address: "Nishi-Ikebukuro 3-chome, Toshima, Tokyo, 171-0021, Japan" },
+  { livehouse: "池袋 LiveHouse mono", address: "South Ikebukuro, Toshima, Tokyo, Japan" },
+  # Saitama
+  { livehouse: "Saitama Super Arena", address: "Saitama Super Arena, Shintoshin, Chuo Ward, Saitama, Saitama Prefecture, 330-0081, Japan" },
+  { livehouse: "HEAVEN'S ROCK さいたま新都心 VJ-3", address: "Kamiochiai 4-chome, Chuo Ward, Saitama, Saitama Prefecture, 338-0001, Japan" },
+  { livehouse: "live 空舞台", address: "Higashi-Iwatsuki 4-chome, Iwatsuki Ward, Saitama, Saitama Prefecture, 339-0065, Japan" },
+  { livehouse: "Narciss", address: "Takasago 2-chome, Urawa Ward, Saitama, Saitama Prefecture, 330-0063, Japan" },
+  { livehouse: "MUSIC & LIVE BAR CITYLIGHTS", address: "Kamiochiai, Chuo Ward, Saitama, Saitama Prefecture, 330-0081, Japan" },
+  { livehouse: "西川口 Live House Hearts", address: "Namiki 2-chome, Iwatsuki Ward, Saitama, Saitama Prefecture, 337-0002, Japan" },
+  { livehouse: "ayers", address: "Tokiwa 9-chome, Urawa Ward, Saitama, Saitama Prefecture, 336-0074, Japan" },
+  # Kichijoji
+  { livehouse: "NEPO", address: "Shimorenjaku 1-chome, Mitaka, Tokyo, 181-0013, Japan" },
+  { livehouse: "吉祥寺 SHUFFLE", address: "Kichijoji Minamicho, Musashino, Tokyo, 180-0003, Japan" },
+  { livehouse: "Silver Elephant", address: "吉祥寺駅, Heiwa Dori, Kichijoji Honcho, Musashino, Tokyo, 180-8520, Japan" },
+  { livehouse: "Black and Blue", address: "Kichijoji-honcho 1-chome, Musashino, Tokyo, 180-0004, Japan" },
+  { livehouse: "Daydream Kichijoji", address: "Kichijoji, Heiwa Dori, Kichijoji-honcho 1-chome, Musashino, Tokyo, 180-8552, Japan" },
+  { livehouse: "Strings", address: "Kichijoji Honcho, Musashino, Tokyo, 180-8520, Japan" },
+  # Koenji
+  { livehouse: "AG22", address: "AG22, 高南通り, Koenji minami 4, Kōenji-south, Kōenji, Suginami, Tokyo, 166-0003, Japan" },
+  { livehouse: "AMP cafe", address: "AMP, Koenji Look Street, Koenji minami 2, Kōenji-south, Kōenji, Suginami, Tokyo, 166-0003, Japan " },
+  { livehouse: "DJ's Bar Cave", address: "DJ's bar Koenji Cave, 高南通り, Koenji minami 4, Kōenji-south, Kōenji, Suginami, Tokyo, 166-0003, Japan" },
+  { livehouse: "Faith", address: "FAITH, Kannana-dori Avenue, Koenji minami 4, Kōenji-south, Kōenji, Suginami, Tokyo, 166-0003, Japan" },
+  { livehouse: "Higashi Koenji U.F.O.CLUB (UFO Club)", address: "Koenji Higashi 2-chome, Kōenji, Suginami, Tokyo, 166-0002, Japan" },
+  { livehouse: "Knock (Koenji)", address: "Knock, 6, Koenji minami 3, Kōenji-south, Kōenji, Suginami, Tokyo, 166-0003, Japan" },
+  { livehouse: "Koenji High", address: "Koenji High, 南中央通り, Koenji minami 4, Kōenji-south, Kōenji, Suginami, Tokyo, 166-0003, Japan" },
+  { livehouse: "Oriental Force", address: "Oriental Force, 第８日東ビル, Koenji minami 3, Kōenji-south, Kōenji, Suginami, Tokyo, 166-0003, Japan" },
+  { livehouse: "Penguin House", address: "PENGUIN HOUSE, 純情商店街, Koenji kita 3, Kōenji-north, Kōenji, Suginami, Tokyo, 166-0002, Japan" },
+  { livehouse: "Roots (Koenji)", address: "ROOTS!, Central Road, Koenji kita 3, Kōenji-north, Kōenji, Suginami, Tokyo, 166-0002, Japan" },
+  { livehouse: "Higashi Koenji U.F.O.CLUB (UFO Club)", address: "Koenji Higashi 2-chome, Kōenji, Suginami, Tokyo, 166-0002, Japan" },
+  { livehouse: "SUBstore Koenji", address: "SUBstore Tokyo, 高円寺中通り, Koenji kita 3, Kōenji-north, Kōenji, Suginami, Tokyo, 166-0002, Japan" },
+  # Yokohama
+  { livehouse: "1000 Club", address: "1000 CLUB, Minamisaiwai 2-chome, Nishi Ward, Yokohama, Kanagawa Prefecture, 220-0005, Japan" },
+  { livehouse: "El Puente ", address: "Minamisengencho, Nishi Ward, Yokohama, Kanagawa Prefecture, 220-0024, Japan" },
+  { livehouse: "KT Zepp Yokohama", address: "KT Zepp Yokohama, 6, Minatomirai 4-chome, Nishi Ward, Yokohama, Kanagawa Prefecture, 220-0012, Japan " },
+  { livehouse: "Pacifico Yokohama", address: "PACIFICO Yokohama North, 2, Minatomirai 1-chome, Nishi Ward, Yokohama, Kanagawa Prefecture, 220-0012, Japan" },
+  { livehouse: " Pia Arena MM", address: "PIA ARENA MM, 1, Nishi Ward, Yokohama, Kanagawa Prefecture, 231-0017, Japan" }
 ]
 
-random_address = address.sample
-
 event_names = ["Sonic Splash", "Groove Blitz", "Echo Wave", "Riff Rampage", "Harmonic Pop", "Bass Boom", "Electro Surge", "Jam Buzz", "Rock Riot", "Folk Flick", "Melody Pop"].shuffle
+
 bands = [
   { band: "Red Velvet", description: 'A powerhouse K-pop girl group known for their unique dual concept of "Red" (bright, bubbly pop) and "Velvet" (smooth R&B). Hits like "Red Flavor" and "Psycho" have solidified their global popularity.', genre: "K-pop" },
   { band: "Arctic Monkeys", description: 'British indie rock band hailing from Sheffield, known for their electrifying live performances and critically acclaimed albums like "AM" and "Whatever People Say I Am, Thats What Im Not".', genre: "Indie Rock" },
@@ -94,17 +194,17 @@ bands.each do |band_info|
   user.save!
 end
 
-# Create gigs for 20 bands
-20.times do |i|
+# Create gig for each livehouse
+livehouses.each do |livehouse|
   band_info = bands.sample # Randomly select a band from the array
   gig = Gig.create!(
     user: band_info[:user], # Associate the user with the gig
     band: band_info[:band],
     time: Faker::Date.between(from: Date.today, to: 30.days.from_now),
     description: band_info[:description],
-    location: address.sample, # Use the random address
-    event_name: event_names.sample,
-    # genre: band[:genre],
+    location: livehouse[:address],
+    location_name: livehouse[:livehouse],
+    event_name: event_names.sample
   )
   gig.genre_list.add(band_info[:genre])
   gig.save!
@@ -117,12 +217,11 @@ tonight_gig = Gig.create!(
   band: band_info[:band],
   time: Date.today, # Set the time to today
   description: band_info[:description],
-  location: address.sample, # Use the random address
-  event_name: event_names.sample,
+  location: livehouses.map { |i| i[:address] },
+  event_name: event_names.sample
 )
 tonight_gig.genre_list.add(band_info[:genre])
 tonight_gig.save!
-
 
 # Fetch all gigs
 gigs = Gig.all
@@ -130,7 +229,7 @@ gigs = Gig.all
 # Iterate over each gig and create a chatroom
 gigs.each do |gig|
   chatroom = gig.build_chatroom(
-    name: "#{gig.event_name}: #{gig.band}",
+    name: "#{gig.event_name}: #{gig.band}"
   )
   if chatroom.save
     puts "Chatroom created for gig: #{gig.event_name} - #{gig.band}"
@@ -144,7 +243,7 @@ users = [
   { email: "senie@senie.com", password: "123456", nickname: "Senie" },
   { email: "dianna@dianna.com", password: "123456", nickname: "Dianna" },
   { email: "yoosun@yoosun.com", password: "123456", nickname: "Yoosun" },
-  { email: "erika@erika.com", password: "123456", nickname: "Erika" },
+  { email: "erika@erika.com", password: "123456", nickname: "Erika" }
 ]
 
 # Create users, attaches profile photo and adds to the user
@@ -170,3 +269,4 @@ end
 puts "Created #{User.count} users!"
 puts "Created #{Gig.count} gigs!"
 puts "Created #{Registration.count} registrations!"
+puts "Created #{Chatroom.count} chatrooms!"
