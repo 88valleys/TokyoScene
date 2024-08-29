1ba538fce9112538f24fe4e96c5fb9ebd8c4ea36
Senie Calalang <senie.c88@gmail.com>
2024-08-29 19:02:59 +0900

    refactor: Update gig search functionality and improve genre retrieval

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,11 +7,15 @@ class PagesController < ApplicationController
     @registration = Registration.new
 
     # Initialize @genres
-    unique_genres = ["K-pop", "Indie Rock", "Soul", "Funk", "Funk Soul", "Alternative Rock", "Pop Rock", "Britpop", "Rock", "Soul Pop", "R&B", "Modern Soul", "Future Soul", "Psychedelic Soul", "Neo-Soul", "Pop", "Emo Rock"]
-    @genres = unique_genres.map { |genre| ActsAsTaggableOn::Tag.find_or_create_by(name: genre) }
 
-    # Search by gig's location and name(event_name)
-    @gigs = @gigs.search_by_name_and_location(params[:query]) if params[:query].present?
+    # Retrievees all the unique genres from the database
+    unique_genres = ActsAsTaggableOn::Tag.for_context("genre").pluck(:name).map(&:downcase).uniq
+    @genres = unique_genres.map { |genre| ActsAsTaggableOn::Tag.find_or_create_by(name: genre) }.sort_by(&:name)
+
+    # Search by gig's location, name, and band
+    if params[:query].present?
+      @gigs = @gigs.search_by_name_and_location(params[:query])
+    end
 
     # recommend gigs
     if current_user
@@ -34,8 +38,7 @@ class PagesController < ApplicationController
       { lat: gig.latitude,
         lng: gig.longitude,
         info_window_html: render_to_string(partial: "gigs/gigs", locals: { gig: gig }),
-        marker_html: render_to_string(partial: "gigs/marker", locals: { gig: gig })
-      }
+        marker_html: render_to_string(partial: "gigs/marker", locals: { gig: gig }) }
     end
 
     flash[:notice] = "There are no results for the search" if @gigs.empty?
@@ -43,20 +46,20 @@ class PagesController < ApplicationController
 
   private
 
-  def filter_by_date(gigs, time)
-    case time
-    when 'tonight'
-      gigs.where('time >= ? AND time < ?', Time.zone.now.beginning_of_day, Time.zone.now.end_of_day)
-    when 'this_weekend'
+  def filter_by_date(gigs, date)
+    case date
+    when "tonight"
+      gigs.where("date >= ? AND date < ?", Time.zone.now.beginning_of_day, Time.zone.now.end_of_day)
+    when "this_weekend"
       end_of_weekend = Time.zone.now.end_of_week(:monday)
-      start_of_weekend = (end_of_weekend - 1.day).beginning_of_day
-      gigs.where('time >= ? AND time < ?', start_of_weekend, end_of_weekend)
-    when 'this_month'
-      gigs.where('time >= ? AND time < ?', Time.zone.now.beginning_of_month, Time.zone.now.end_of_month)
-    when 'next_month'
+      start_of_weekend = (end_of_weekend - ((end_of_weekend.wday - 5) % 7).days).beginning_of_day
+      gigs.where("date >= ? AND date < ?", start_of_weekend, end_of_weekend)
+    when "this_month"
+      gigs.where("date >= ? AND date < ?", Time.zone.now.beginning_of_month, Time.zone.now.end_of_month)
+    when "next_month"
       start_of_next_month = Time.zone.now.next_month.beginning_of_month
       end_of_next_month = start_of_next_month.end_of_month
-      gigs.where('time >= ? AND time < ?', start_of_next_month, end_of_next_month)
+      gigs.where("date >= ? AND date < ?", start_of_next_month, end_of_next_month)
     else
       gigs
     end
