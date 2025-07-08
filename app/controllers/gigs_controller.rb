209241a07e958aa18decd116c84010c0a2ec4560
Senie Calalang <senie.c88@gmail.com>
2025-07-08 22:29:32 +0900

    fix: update actions

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,9 +1,11 @@
 class GigsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def create
     @gig = Gig.new(gig_params)
     if @gig.save
       @gig.fetch_band_image
-      redirect_to @gig, notice: 'Gig was successfully created.'
+      redirect_to @gig, notice: "Gig was successfully created."
     else
       render :new
     end
@@ -13,12 +15,12 @@ class GigsController < ApplicationController
     @gig = Gig.find(params[:id])
     if @gig.update(gig_params)
       @gig.fetch_band_image
-      redirect_to @gig, notice: 'Gig was successfully updated.'
+      redirect_to @gig, notice: "Gig was successfully updated."
     else
       render :edit
     end
   end
-  
+
   def show
     @gig = Gig.find(params[:id])
     @registration = Registration.new
